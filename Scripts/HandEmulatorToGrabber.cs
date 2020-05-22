83d95bac1b859bf86b5953996f609fe4dcad1099
Oxters Wyzgowski <oxters168@gmail.com>
2020-05-23 01:30:52 +0300

    Created simple HandEmulatorToGrabber script, will make better soon.

---
+++ Scripts/HandEmulatorToGrabber.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityHelpers;
+
+namespace VRPhysicsHands
+{
+    public class HandEmulatorToGrabber : MonoBehaviour
+    {
+        public Grabber grabber;
+        public HandEmulator currentHand;
+        public OVRHand.Hand handType;
+
+        void Start()
+        {
+            grabber.AddGrabSpot("Palm");
+        }
+        void Update()
+        {
+            grabber.SetGrabSpotDimensions("Palm", new SpherecastInfo()
+            {
+                position = currentHand.palm.position,
+                direction = currentHand.palm.forward,
+                distance = 0.1f,
+                radius = 0.05f,
+                castMask = ~0
+            });
+            grabber.SetGrabSpotGrabbing("Palm", OVRInput.Get(handType == OVRHand.Hand.HandRight ? OVRInput.RawButton.RHandTrigger : OVRInput.RawButton.LHandTrigger, OVRInput.Controller.All));
+        }
+    }
+}
