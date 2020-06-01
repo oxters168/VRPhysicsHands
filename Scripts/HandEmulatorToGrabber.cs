d891c200aba0b6af53912418da6246af6ba7f663
Oxters Wyzgowski <oxters168@gmail.com>
2020-06-01 21:23:43 +0300

    Changed from SpherecastInfo to RaycastInfo in HandEmulatorToGrabber.

--- Scripts/HandEmulatorToGrabber.cs
+++ Scripts/HandEmulatorToGrabber.cs
@@ -8,8 +8,8 @@ namespace VRPhysicsHands
         public Grabber grabber;
         public HandEmulator currentHand;
         public OVRHand.Hand handType;
-        BoneId[] fingerTipIds = new BoneId[] { BoneId.Hand_ThumbTip, BoneId.Hand_IndexTip, BoneId.Hand_MiddleTip, BoneId.Hand_RingTip, BoneId.Hand_PinkyTip };
-        string[] grabSpotNames = new string[] { "Palm", "IndexThumbPinch", "MiddleThumbPinch", "RingThumbPinch", "PinkyThumbPinch" };
+        BoneId[] fingerTipIds = new BoneId[] { BoneId.Hand_IndexTip, BoneId.Hand_MiddleTip, BoneId.Hand_RingTip, BoneId.Hand_PinkyTip };
+        string[] grabSpotNames = new string[] { "IndexThumbPinch", "MiddleThumbPinch", "RingThumbPinch", "PinkyThumbPinch" };
 
         void Start()
         {
@@ -21,32 +21,20 @@ namespace VRPhysicsHands
         }
         void Update()
         {
-            var thumbPosition = currentHand.bones[(int)fingerTipIds[0]].mesh.position;
-            for (int i = 1; i < fingerTipIds.Length; i++)
+            var thumbPosition = currentHand.bones[(int)BoneId.Hand_ThumbTip].mesh.position;
+            for (int i = 0; i < fingerTipIds.Length; i++)
             {
                 var currentFingerPosition = currentHand.bones[(int)fingerTipIds[i]].mesh.position;
                 var difference = currentFingerPosition - thumbPosition;
                 float distance = difference.magnitude;
-                //Vector3 midpoint = difference / 2;
                 Vector3 direction = difference.normalized;
-                grabber.SetGrabSpotDimensions(grabSpotNames[i], new SpherecastInfo()
+                grabber.SetGrabSpotDimensions(grabSpotNames[i], new RaycastInfo()
                 {
                     position = thumbPosition,
                     direction = direction,
-                    distance = distance / 2f,
-                    radius = distance < 0.1f ? distance / 2f : 0,
-                    castMask = ~0
+                    distance = distance,
                 });
             }
-            /*grabber.SetGrabSpotDimensions("Palm", new SpherecastInfo()
-            {
-                position = currentHand.palm.position,
-                direction = currentHand.palm.forward,
-                distance = 0.1f,
-                radius = 0.05f,
-                castMask = ~0
-            });
-            grabber.SetGrabSpotGrabbing("Palm", OVRInput.Get(handType == OVRHand.Hand.HandRight ? OVRInput.RawButton.RHandTrigger : OVRInput.RawButton.LHandTrigger, OVRInput.Controller.All));*/
         }
     }
 }
